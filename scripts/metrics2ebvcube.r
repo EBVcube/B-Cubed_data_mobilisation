b291b5882bd0f3bcbde98815607dea8c5ba36cfe
Luise Quoß <luise.quoss@posteo.de>
2024-07-02 10:42:35 +0200

    changed temp. res. in json, added csv with taxonomy formatted for ebvcube, changed creation-script

--- scripts/metrics2ebvcube.r
+++ scripts/metrics2ebvcube.r
@@ -9,28 +9,21 @@ library(terra)
 # What is the difference between the dictionary and taxonomy files?
 # error producing ebv_create_taxonomy
 
-
 #paths
-id <- 1 #modify
-# root <- paste0('I:\\biocon\\Quoss_Luise\\netcdf\\netcdf_creation\\ebv_portal\\', id)
-json <- file.path(here("input/ebvformat/json/metrics_dates.json")) #root, 'metadata_v2.json')
-# csv <- file.path(root, paste0('entities_',id,'.csv'))
+json <- file.path(here("input/ebvformat/json/metrics_dates.json"))
 out <- file.path(here("output/datacubes/nc/IAS_dates_metrics.nc")) # modify
-
 #tifs
 tif_root <- here("output/datacubes/tiff_metrics/")
-# tif_folders <- file.path(list.dirs(tif_root, recursive = F), 'AOH_land_use_only')
-# dict <- file.path(root, 'dictionary.csv')
-tax <- file.path(here("input/data/ias_10/List87IAS_EU_match_gbif_synonyms.csv"))
-dict <- tax
+#taxonomy file
+tax <- file.path(here("input/data/ias_10/List87IAS_EU_match_gbif_synonyms_ebvcube.csv"))
 
 # define raster settings
 r <- rast(here("output/datacubes/tif_metrics/ias_earliest_date_records.tif")) # reference raster
 extent <- c(ext(r)[1], ext(r)[2], ext(r)[3], ext(r)[4])
 res <- res(r)
-fillvalue <- NA
-prec <- 'INT4U'
-epsg <- 3035 #crs(r)
+fillvalue <- NaN
+prec <- 'integer'
+epsg <- 3035
 sep <- ','
 
 #create empty file
@@ -41,20 +34,27 @@ ebv_create_taxonomy(jsonpath = json,
            extent = extent,
            resolution = res,
            fillvalue = fillvalue,
-           prec = 'float',
-           sep = sep
+           prec = prec,
+           sep = sep,
+           overwrite=T
 )
-# lsid = FALSE,
-#            force_4D = TRUE,
-#            overwrite = TRUE,
-#            verbose = TRUE)
+
 
 ebv_datacubepaths(out)
-ebv_properties(out, 'scenario_1/metric')
+ebv_properties(out, metric=1)
 
 #add data -----
-#read dictionary with taxon keys
-dict_data <- read.csv(dict)
-
 #get entity names
 entity_names <- ebv_properties(out, verbose=F)@general$entity_names
+
+#i don't know which layer belongs to which species so the data is is not mapped correctly to the entity names (species)!! 
+for(i in 1:77){
+  ebv_add_data(filepath_nc = out,
+               data = terra::as.matrix(r[[i]], wide=T),
+               band = i,
+               metric = 1,
+               entity = i,
+               timestep = 1,
+               ignore_RAM = T)
+}
+
